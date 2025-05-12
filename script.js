3ee8278c9ac7b175aa01ed83dbe9e358e4c7cc9e
odnath <od.nathmnair1@gmail.com>
2025-05-12 19:14:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    const charCount = {};
+
+    // Count the frequency of each character
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    // Find the first character with count 1
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
